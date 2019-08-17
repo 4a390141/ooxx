b6e93f9c513c27db01dc6216a8bec486e4ef0231
4a390141 <4a390141@stust.edu.tw>
2019-08-18 00:14:51 +0800

    ooxx start

---
+++ src/components/LeaveComments.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="leaveComments">
+    <div>
+      <input type="text">
+      <button>送出</button>
+    </div>
+    <ul>
+      <li></li>
+    </ul>
+  </div>
+</template>
+<script src="https://www.gstatic.com/firebasejs/6.4.0/firebase-app.js"></script>
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCgW5dRMaPyjm_f2Q6_fo3UgLC7V2m-Ylg",
+    authDomain: "ooxx-73434.firebaseapp.com",
+    databaseURL: "https://ooxx-73434.firebaseio.com",
+    projectId: "ooxx-73434",
+    storageBucket: "",
+    messagingSenderId: "559671550450",
+    appId: "1:559671550450:web:e1b9a6f3459c2dbb"
+  }
+  firebase.initializeApp(firebaseConfig)
+</script>
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'leaveComments',
+  components: {
+  }
+}
+</script>
