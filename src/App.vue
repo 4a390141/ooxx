bd107ae64d38a2055c6ed90925e3a85cceb3c829
4a390141 <4a390141@stust.edu.tw>
2019-08-18 16:04:15 +0800

    update dist v8

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div id="nav">
       <b-navbar toggleable="lg" type="dark" variant="info">
         <!-- <b-navbar-brand href="#">NavBar</b-navbar-brand> -->
-        <router-link to="./">Home</router-link>
+        <router-link to="/">Home</router-link>
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
           <!-- <b-navbar-nav>
@@ -12,7 +12,7 @@
           <!-- Right aligned nav items -->
           <b-navbar-nav class="ml-auto">
             <b-nav-item right>
-              <router-link to="./leaveComments">留言</router-link>
+              <router-link to="/leaveComments">留言</router-link>
             </b-nav-item>
             <b-nav-item-dropdown right>
               <!-- Using 'button-content' slot -->
